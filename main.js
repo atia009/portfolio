813c7c24df86c9d17ab58a7bb7af4d845aafacb0
atia009 <aarontia009@gmail.com>
2022-04-19 09:16:52 -0700

    Create project objects from constructor

--- main.js
+++ main.js
@@ -17,4 +17,22 @@ Project.prototype = {
     }
 }
 
+// objects
+const project1 = new Project(`Essential Aesthetics`, `01-project.png`, `https://essentialaesthetics.netlify.app`, `https://github.com/atia009/essential-aesthetics`);
+project1.addDesc(`Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam amet molestiae at dicta architecto sit enim inventore repellat tempora eaque`);
+project1.addTech(`HTML5`);
+project1.addTech(`CSS3`);
+project1.addTech(`JavaScript`);
 
+const project2 = new Project(`Protagonist`, `01-project.png`, `https://protagonistco.netlify.app`, `https://github.com/atia009/protagonist`);
+project2.addDesc(`Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam amet molestiae at dicta architecto sit enim inventore repellat tempora eaque`);
+project2.addTech(`HTML5`);
+project2.addTech(`CSS3`);
+project2.addTech(`JavaScript`);
+project2.addTech(`Bootstrap`);
+
+const project3 = new Project(`Disflex`, `01-project.png`, `https://disflex.netlify.app`, `https://github.com/atia009/disflex`);
+project3.addDesc(`Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam amet molestiae at dicta architecto sit enim inventore repellat tempora eaque`);
+project3.addTech(`HTML5`);
+project3.addTech(`CSS3`);
+project3.addTech(`JavaScript`);
