e86cc2ace428ea18d06e9f62a8f766dd8c650fb9
atia009 <aarontia009@gmail.com>
2022-04-19 09:15:54 -0700

    Add tech property to constructor and prototype methods

--- main.js
+++ main.js
@@ -4,25 +4,17 @@ function Project(title, img, demo, github) {
     this.img = img;
     this.demo = demo;
     this.github = github
+    this.tech = [];
 }
 
 // prototypes 
-Project.prototype.addDesc = function(desc) {
-    this.desc = desc;
+Project.prototype = {
+    addDesc: function(desc) {
+        this.desc = desc;
+    },
+    addTech: function(tech) {
+        this.tech.push(tech);
+    }
 }
 
-//// add array to Project object constructor for the technologies used
 
-
-// objects
-const project1 = new Project(`Essential Aesthetics`, `01-project.png`, `https://essentialaesthetics.netlify.app`, `https://github.com/atia009/essential-aesthetics`);
-const project2 = new Project(`Protagonist`, `01-project.png`, `https://protagonistco.netlify.app`, `https://github.com/atia009/protagonist`);
-const project3 = new Project(`Disflex`, `01-project.png`, `https://disflex.netlify.app`, `https://github.com/atia009/disflex`);
-
-
-// get project property
-// add project 
-// functions
-console.log(project1);
-project1.addDesc("hello");
-console.log(project1);
