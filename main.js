47406a64785668970cd3b06d94dd0e54e2b772f9
atia009 <aarontia009@gmail.com>
2022-05-21 16:31:08 -0700

    Add aria-label and title to icons

--- main.js
+++ main.js
@@ -68,12 +68,12 @@ function generateProjectsMarkup() {
                     <ul class="tech">
                     ${generateTechsMarkup(project.tech)}</ul>
                     <ul class="external">
-                        <a href="${project.github}" target="__blank" class="external__link">
+                        <a href="${project.github}" target="__blank" class="external__link" aria-label="GitHub" title="GitHub">
                         <svg class="external__icon" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="24" height="24" viewBox="0 0 24 24">
                             <path d="M12,2A10,10 0 0,0 2,12C2,16.42 4.87,20.17 8.84,21.5C9.34,21.58 9.5,21.27 9.5,21C9.5,20.77 9.5,20.14 9.5,19.31C6.73,19.91 6.14,17.97 6.14,17.97C5.68,16.81 5.03,16.5 5.03,16.5C4.12,15.88 5.1,15.9 5.1,15.9C6.1,15.97 6.63,16.93 6.63,16.93C7.5,18.45 8.97,18 9.54,17.76C9.63,17.11 9.89,16.67 10.17,16.42C7.95,16.17 5.62,15.31 5.62,11.5C5.62,10.39 6,9.5 6.65,8.79C6.55,8.54 6.2,7.5 6.75,6.15C6.75,6.15 7.59,5.88 9.5,7.17C10.29,6.95 11.15,6.84 12,6.84C12.85,6.84 13.71,6.95 14.5,7.17C16.41,5.88 17.25,6.15 17.25,6.15C17.8,7.5 17.45,8.54 17.35,8.79C18,9.5 18.38,10.39 18.38,11.5C18.38,15.32 16.04,16.16 13.81,16.41C14.17,16.72 14.5,17.33 14.5,18.26C14.5,19.6 14.5,20.68 14.5,21C14.5,21.27 14.66,21.59 15.17,21.5C19.14,20.16 22,16.42 22,12A10,10 0 0,0 12,2Z" />
                         </svg>
                         </a>
-                        <a href="${project.demo}" target="__blank" class="external__link">
+                        <a href="${project.demo}" target="__blank" class="external__link" aria-label="Demo" title="Demo">
                         <svg class="external__icon" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="24" height="24" viewBox="0 0 24 24">
                         <path d="M19 3C20.1 3 21 3.9 21 5V19C21 20.1 20.1 21 19 21H5C3.9 21 3 20.1 3 19V5C3 3.9 3.9 3 5 3H19M8.7 8L10.8 10.1L7 14L9.8 16.8L13.6 12.9L15.7 15V8H8.7Z" />
                         </svg>
@@ -154,10 +154,25 @@ function startColorModeFunctionality() {
 }
 
 function updateColorMode() {
+    updateColorLabel();
     updateRootVariable(`--primary-toggle`, `#1c1d25`, `#ebebeb`);
     updateRootVariable(`--secondary-toggle`, `#b0b2c3`, `#4c4f65`);
     updateRootVariable(`--hover-toggle`, `#ffffff`, `#1c1d25`);
     updateRootVariable(`--txt-toggle`, `#ffffff`, `#4c4f65`);
+}
+
+function updateColorLabel() {
+    updateAttributeValue(`aria-label`, `Light Mode`, `Dark Mode`);
+    updateAttributeValue(`title`, `Light Mode`, `Dark Mode`);
+}
+
+function updateAttributeValue(name, value1, value2) {
+    const color = document.querySelector(`.color-mode`);
+    if (color.getAttribute(name) === `Light Mode`) {
+        color.setAttribute(name, `Dark Mode`);
+    } else {
+        color.setAttribute(name, `Light Mode`);
+    }
 }
 
 function addClassToElement(element, className) {
